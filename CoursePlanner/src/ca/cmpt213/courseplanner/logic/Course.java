d2d2400d44ceb23e472ef3557d780ffcba5da506
KevinTran <kytran@sfu.ca>
2016-07-27 11:43:32 -0700

    Added template file for Courses. Contains basic ability to read from csv file. May need to integrate manually into IntelliJ

---
+++ CoursePlanner/src/ca/cmpt213/courseplanner/logic/Course.java
@@ -0,0 +1,38 @@
+package ca.cmpt213.courseplanner.logic;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/* Contains all information regarding courses.
+ * The course information is initially loaded from a CSV file */
+public class Course {
+
+	public Course() {
+
+	}
+
+	public static void loadFromCSV(String directory) {
+		try{
+			Scanner csv = new Scanner(new File(directory));
+			while(csv.hasNextLine()){
+				String data = csv.nextLine();
+				String[] columns = data.split(",");
+				for (int i=0; i<columns.length; i++) {
+					System.out.print(columns[i]);
+				}
+				System.out.println();
+			}
+			csv.close();
+		}catch (FileNotFoundException e){
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		String directory = "/home/kytran/sfuhome/cmpt213/cmpt213a5/CoursePlanner" +
+				"/src/ca/cmpt213/courseplanner/logic/course_data_2016.csv";
+		loadFromCSV(directory);
+	}
+
+}
