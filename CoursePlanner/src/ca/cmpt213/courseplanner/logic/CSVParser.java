9a2d25b0e9bd2e41174e78397fb4153dafa0eabb
ktraain <kevinyokutran@gmail.com>
2016-07-27 19:10:09 -0700

    Added CSV parsing into hashmap

---
+++ CoursePlanner/src/ca/cmpt213/courseplanner/logic/CSVParser.java
@@ -0,0 +1,49 @@
+package ca.cmpt213.courseplanner.logic;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Handle loading data from the CSV
+ * Source for RegEx used:
+ * http://stackoverflow.com/questions/1757065/java-splitting-a-comma-separated-string-but-ignoring-commas-in-quotes
+ */
+public class CSVParser {
+
+	private final static String SPLIT_REGEX = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
+	private final static String directory = "data/course_data_2016.csv";
+
+	public void loadCoursesFromCSV() {
+		int count = 0;
+		try{
+			Scanner csv = new Scanner(new File(directory));
+			String[] headers = csv.nextLine().split(SPLIT_REGEX, -1);
+			while(csv.hasNextLine()){
+				Course course = new Course();
+				String data = csv.nextLine();
+				String[] columns = data.split(SPLIT_REGEX, -1);
+				for (int i=0; i<columns.length; i++) {
+					course.setInfo(headers[i], columns[i]);
+				}
+				course.addToCourseMap(course);
+
+			}
+			System.out.println("Successfully parsed " + count + " entries from CSV");
+			csv.close();
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
+		}
+	}
+
+	/*  Used for testing */
+	public static void main(String[] args) {
+		CSVParser parser = new CSVParser();
+		parser.loadCoursesFromCSV();
+		List<Course> cmptCourses = Course.courses.get("CMPT");
+		System.out.println(cmptCourses.get(0).getCatalogNumber());
+	}
+
+}
--- CoursePlanner/src/ca/cmpt213/courseplanner/logic/Course.java
+++ CoursePlanner/src/ca/cmpt213/courseplanner/logic/Course.java
@@ -1,37 +1,104 @@
 package ca.cmpt213.courseplanner.logic;
 
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 /* Contains all information regarding courses.
- * The course information is initially loaded from a CSV file */
+ * The course information is initially loaded from a CSV file
+ * */
 public class Course {
 
-	public Course() {
+	public static HashMap<String,ArrayList<Course>> courses = new HashMap<>();
 
+	private String semester;
+	private String subject;
+	private String catalogNumber;
+	private String location;
+	private String enrollmentCapacity;
+	private String enrollmentTotal;
+	private ArrayList<String> instructors;
+	private String componentCode;
+
+
+	public Course() {
+		this.instructors = new ArrayList<>();
 	}
 
-	public static void loadFromCSV(String directory) {
-		try{
-			Scanner csv = new Scanner(new File(directory));
-			while(csv.hasNextLine()){
-				String data = csv.nextLine();
-				String[] columns = data.split(",");
-				for (int i=0; i<columns.length; i++) {
-					System.out.print(columns[i]);
-				}
-				System.out.println();
-			}
-			csv.close();
-		}catch (FileNotFoundException e){
-			e.printStackTrace();
+	public static void addToCourseMap(Course course) {
+		if (!Course.courses.containsKey(course.getSubject())) {
+			Course.courses.put(course.getSubject(), new ArrayList<>());
+		}
+		else {
+			ArrayList<Course> courseList = Course.courses.get(course.getSubject());
+			courseList.add(course);
 		}
 	}
 
-	public static void main(String[] args) {
-		String directory = "data/course_data_2016.csv";
-		loadFromCSV(directory);
+	// Getters
+	public String getSubject() {
+		return this.subject;
+	}
+	public String getCatalogNumber() {
+		return this.catalogNumber;
+	}
+
+	// Setters
+	public void setInfo(String header, String columnData) {
+		switch(header) {
+			case "SEMESTER":
+				setSemester(columnData);
+				break;
+			case "SUBJECT":
+				setSubject(columnData);
+				break;
+			case "CATALOGNUMBER":
+				setCatalogNumber(columnData);
+				break;
+			case "LOCATION":
+				setLocation(columnData);
+				break;
+			case "ENROLLMENTCAPACITY":
+				setEnrollmentCapacity(columnData);
+				break;
+			case "ENROLLMENTTOTAL":
+				setEnrollmentTotal(columnData);
+			case "INSTRUCTORS":
+				setInstructors(columnData);
+				break;
+			case "COMPONENTCODE":
+				setComponentCode(columnData);
+				break;
+		}
+	}
+	private void setSemester(String semester) {
+		this.semester = semester;
+	}
+	private void setSubject(String subject) {
+		this.subject = subject;
+	}
+	private void setCatalogNumber(String catalogNumber) {
+		this.catalogNumber = catalogNumber;
+	}
+	private void setLocation(String location) {
+		this.location = location;
+	}
+	private void setEnrollmentCapacity(String capacity) {
+		this.enrollmentCapacity = capacity;
+	}
+	private void setEnrollmentTotal(String total) {
+		this.enrollmentTotal = total;
+	}
+	private void setInstructors(String instructors) {
+		for (String instructor : instructors.split(",")) {
+			instructor = instructor.replace("\"", "");
+			instructor = instructor.trim();
+			this.instructors.add(instructor);
+		}
+	}
+	private void setComponentCode(String code) {
+		this.componentCode = code;
 	}
 
 }
