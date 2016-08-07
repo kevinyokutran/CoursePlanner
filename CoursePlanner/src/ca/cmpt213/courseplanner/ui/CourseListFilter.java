4d71ff794e4eeb7a10d7dfcfb44f22c070e1ef32
Josh Vocal <joshvocal@gmail.com>
2016-08-06 18:10:42 -0700

    ui skeleton for courseListFilter

--- CoursePlanner/src/ca/cmpt213/courseplanner/ui/CourseListFilter.java
+++ CoursePlanner/src/ca/cmpt213/courseplanner/ui/CourseListFilter.java
@@ -24,9 +24,61 @@ public class CourseListFilter extends BasePanel{
 	@Override
 	protected JPanel setPanel() {
 		panel = new JPanel();
-		panel.setBackground(Color.WHITE);
+		//panel.setBackground(Color.WHITE);
 		panel.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED,
 				Color.BLACK, Color.GRAY));
+		panel.add(departmentList());
+		panel.add(undergraduateCheckbox());
+		panel.add(graduateCheckbox());
+		panel.add(updateCourseList());
 		return panel;
 	}
+
+	private JPanel departmentList() {
+		// Test String of list for courses
+		String[] petStrings = {"Bird", "Cat", "Dog", "Rabbit", "Pig"};
+
+		JPanel departmentPanel = new JPanel();
+		departmentPanel.add(new JLabel("Department"));
+
+		final JComboBox<String> departmentList = new JComboBox(petStrings);
+		departmentPanel.add(departmentList);
+
+		return departmentPanel;
+	}
+
+	private JPanel undergraduateCheckbox() {
+		JPanel undergraduateCheckboxPanel = new JPanel();
+		final JCheckBox checkBox = new JCheckBox();
+		undergraduateCheckboxPanel.setLayout(new BoxLayout(undergraduateCheckboxPanel,
+				BoxLayout.LINE_AXIS));
+
+		undergraduateCheckboxPanel.add(checkBox);
+		undergraduateCheckboxPanel.add(new JLabel("Include undergrad courses"));
+
+		return undergraduateCheckboxPanel;
+	}
+
+	private JPanel graduateCheckbox() {
+		JPanel graduateCheckboxPanel = new JPanel();
+		final JCheckBox checkBox = new JCheckBox();
+		graduateCheckboxPanel.setLayout(new BoxLayout(graduateCheckboxPanel,
+				BoxLayout.LINE_AXIS));
+
+		graduateCheckboxPanel.add(checkBox);
+		graduateCheckboxPanel.add(new JLabel("Include undergrad courses"));
+
+		return graduateCheckboxPanel;
+	}
+
+	private JPanel updateCourseList() {
+		JPanel updateCourseListPanel = new JPanel();
+		final JButton updateCourseListButton = new JButton("Update Course List");
+		updateCourseListButton.setLayout(new BoxLayout(updateCourseListButton,
+				BoxLayout.LINE_AXIS));
+		updateCourseListPanel.add(updateCourseListButton);
+
+		return updateCourseListPanel;
+	}
+
 }
